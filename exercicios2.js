3b33ec72b70e4f4792587aff05ef0d4b278cfcf6
nickzinhoS2 <102757773+nickzinhoS2@users.noreply.github.com>
2022-05-05 15:42:19 -0300

    Add files via upload

---
+++ exercicios2.js
@@ -0,0 +1,32 @@
+//exercicios
+
+//1-
+function nome (a){
+    console.log (a);
+    return;
+}
+
+nome('Nicolas Pereira');
+
+//2-
+function adição(a, b){
+    let res = a + b;
+    console.log(res);
+    return;
+}
+adição(80, 5);
+
+//3
+function multiplicação(a, b) {
+    let  mult = a+b;
+    console.log(mult);
+    return;
+}
+multiplicação(50, 70);
+
+//4
+ function media (a,b,c,d,){
+     let res = (a+b+c+d)/4;
+     console.log(res);
+ } 
+ media(10,5,8,9)
